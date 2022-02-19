3ec3dbdd99782b08ee57e5f0c3d8719dd252efc3
pacxel09 <83786961+pacxel09@users.noreply.github.com>
2022-02-19 00:12:51 -0600

    Add files via upload

---
+++ parc1 ejer 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int numero,dato = 5;
+    cout<<"digite un numero \n" ;
+    cin>>numero;
+    if (numero<=dato){
+        cout<<"el numero es menor o igual que 5";
+    }
+    else{
+        cout<<"el numero es mayor a  ";
+    }
+
+    return 0;
+}
