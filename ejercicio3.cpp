65b058a33636dd7e69c1cc247651dceb132ec031
pacxel09 <83786961+pacxel09@users.noreply.github.com>
2022-02-19 22:50:13 -0600

    Add files via upload

---
+++ ejercicio3.cpp
@@ -0,0 +1,86 @@
+//system("color F0");
+// ejercicio 1
+/*
+#include <iostream>
+#include <cstdlib>
+int main(void)
+{
+    std::cout<<"hola mundo"<<std::endl<<"hello";
+    std::cout<<std::endl;
+    return  0;
+}
+//*/
+
+//Ejercicio 2
+/*
+#include<iostream>
+int main(void)
+{
+    int a=10;
+    std::cout<<"hola mundo"<<std::endl;//despues del texto "" no lleva punto y coma y termina con endl en el mismo
+    std::cout<<"hello"<<std::endl;
+    std::cout<<"a:";
+    std::cout<<a<<std::endl;
+   // despues del texto "" y el punto y coma
+    //se tiene que saltar la linea de codigo  y ; y <<
+    //no van seguidos
+
+    return 0;
+}
+*/
+
+//Ejercicio 3
+/*
+#include<iostream>
+int main(void){
+    int a,b,s;
+    a=2;
+    std::cout<<a<<std::endl;
+    a=1;
+    std::cout<<"valor de a";
+    std::cout<<a<<std::endl;
+    b=2;
+    std::cout<<"valor:"<<b;
+    std::cout<<std::endl;
+    s=a+b;
+    std::cout<<"valor de suma:"<<a<<"+"<<b<<std::endl;
+    std::cout<<std::endl<<"es:"<<a<<std::endl;
+    return 0;
+}
+*/
+//ejercicio 4
+//*
+//*/
+//ejercicio 5
+//*
+#include<iostream>
+int main(void){
+    system("color F0");
+    int a,b,s;
+    std::cout<<"hola mundo"<<std::endl;
+    std::cout<<"hola mundo"<<std::endl;
+    system("pause");
+    system("cls");
+    a=1;
+    std::cout<<"valor de a:"<<a;
+    std::cout<<std::endl;
+    system("pause");
+    system("cls");
+    a=2;
+    std::cout<<"valor: "<<std::endl;
+    std::cout<<std::endl;
+    system("pause");
+    system("cls");
+    std::cout<<"dame un valor: "<<std::endl;
+    std::cin>>a;
+    std::cout<<"valor de a: "<<a;
+    std::cout<<"dame otro valor"<<std::endl;
+    std::cin>>b;
+    std::cout<<"valor de b: "<<b<<std::endl;
+    s=a+b;
+    std::cout<<"suma es: "<<a + b<<std::endl;
+    std::cout<<a<<"+"<<b<<std::endl;
+    system("pause");
+    return 2020;
+}
+//*/
