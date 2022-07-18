097aa41b3665b8f130c6ac2a04184a4fb693987f
pacxel09 <83786961+pacxel09@users.noreply.github.com>
2022-07-17 22:00:36 -0500

    Add files via upload

---
+++ ejercicio 7.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <conio.h>
+
+using namespace std;
+
+
+
+void portada ()
+{
+cout<<"\n\n                    INSTITUTO POLITECNICO NACIONAL ";
+cout<<"\n\n           Escuela Siperior de Ingenieria Mecanica y electrica";
+cout<<"\n\n                            ESIME Zacatenco";
+cout<<"\n\n              Ingenieria en Comunicaciones y Electronica";
+cout<<"\n\n                       Programacion orientada a objetos";
+cout<<"\n\n                            2AV8";
+cout<<"\n\n   Nombre:Tinoco Martinez Axel Javier";
+getch ();
+}
+
+void datos():
+    float vol,res,corr;
+    int c;
+    int main(void)
+
+int main ()
+{
+portada();
+return 0;
+}
+
+
+    {
+        int
+        datos();
+        switch(c)
+        {
+            case 1:std::cout<<"ingresa el voltaje"<<std::endl;
+            std::cin>>vol;
+            std::cout<<"ingresa la intensidad"<<std::endl;
+            std::cin>>corr;
+            resi=vol/corr;
+            std::cout<<"la resistencia  es"<<std::endl;
+            std::cout<<resi<<std::end;break
+            case 2:std::cout<<"ingresa la intensidad"<<std::endl;
+            std::cin>>corr;
+            std::cout<<"ingrese la resistencia  "<<std::endl;
+            std::cin>>res;
+            volt= corr*res;
+            std::cout<<"el voltaje es"<<std::endl;
+            std::cout<<volt<<std::end;break
+            case 3:std::cout<<"ingrese el voltaje"<<std::endl;
+            std::cin>>vol;
+            std::cout<<"ingrese la resistencia"<<std::endl;
+            std::cin>>res;
+            inte=vol/res;
+            std::cout<<"la intensidad es"<<std::endl;
+            std::cout<<inte<<std::end;break
+        }
+        return 0;
+    }
+
+    void datos()
+    {
+        std::cout<<"ingrese el valor 1"<<std::endl;
+        std::cin>>a;
+        std::cout<<"ingrese el valor 2"<<std::endl;
+        std::cin>>b;
+        std::cout<<"ingrese la operacion"<<std::endl;
+        std::cin>>c;
+    }
